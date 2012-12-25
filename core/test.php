b6b6666edec15823a92d71cebf9908994ec8bcbd
saruhei <saruhei19892046@gmail.com>
2012-12-25 17:04:33 +0900

    ridirect

--- core/test.php
+++ core/test.php
@@ -2,4 +2,10 @@
 require_once "../model/Login.php";
 
 $a = new Login();
-$a->user_registration();
+$b = $a->userRegistration("user2","user2","saruhei1989@moegi.waseda.jp");
+
+/*if(mb_send_mail("saruhei1989@hotmail.com","TESTMAIL","This is a testmassage","From@saruhei1989@hotmail.com")){
+    print("メールは送信されました");
+}else{
+    print("送信失敗");
+}*/
--- model/Login.php
+++ model/Login.php
@@ -4,18 +4,50 @@ require_once "../core/DbManager.php";
 
 class Login{
 
-    public function user_registration($user_name,$user_pass,$user_email){
+    public function userRegistration($user_name,$user_pass,$user_email){
 
         $Login = new DbManager();
         $dbh = $Login->connectMySQL();
-        print_r($dbh);
-        $stmt = $dbh->prepare("select * from user;");
-        $stmt->execute();
-        $result = $stmt->fetchAll();
+        if($this->checkUserNameRepetition($user_name)){
+            //エラーページをリロード
+        }else{
+            if(isset($user_name) && isset($user_pass) && isset($user_email)){
+                /*
+                $stmt = $dbh->prepare("insert into user (user_name,password,email) value (:user_name, :password, :email);");
+                $stmt->bindValue(':user_name',$user_name,PDO::PARAM_STR);
+                $stmt->bindValue(':password',$user_pass,PDO::PARAM_STR);
+                $stmt->bindValue(':email',$user_email,PDO::PARAM_STR);
+                $stmt->execute();
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+                $dbh = null;
+                 */
+                return true;
+            }
+        }
 
         $dbh = null;
 
-        return $result;
+        return false;
+
+    }
+
+
+    public function checkUserNameRepetition($user_name){
+
+        $Login = new DbManager();
+        $dbh = $Login->connectMySQL();
+        $stmt = $dbh->prepare("select user_name from user where user_name = :user_name;");
+        $stmt->bindValue(':user_name',$user_name,PDO::PARAM_STR);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $dbh = null;
+
+        if(isset($result['user_name'])){
+            return true;
+        }else{
+            return false;
+        }
     }
 
 }
