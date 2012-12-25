f6836ea421085e3d668e5cf13c8b5088e50a74bd
tooooomin <kia.xs2x.4b8sw@gmail.com>
2012-12-25 16:59:01 +0900

    view

--- view/mypage.php
+++ view/mypage.php
@@ -15,23 +15,162 @@
 				<ul class="nav nav-pills pull-right">
 					<li class="active"><a href="#">Home</a></li>
 					<li><a href="#">About</a></li>
-					<li><a href="#">Contact</a></li>
 				</ul>
-				<h3 class="muted">Project name</h3>
+				<h3 class="muted">mimikaki</h3>
 			</div>
 			<hr>
 			<h1>みみかきおしらせったー</h1>
 			<input type="text" placeholder="お知らせしてほしい内容">
-			<select>
+		
+			<p>
+			<select class="select_day">
+				<option>なし</option>
 				<option>1</option>
 				<option>2</option>
 				<option>3</option>
 				<option>4</option>
 				<option>5</option>
+				<option>6</option>
+				<option>7</option>
+				<option>8</option>
+				<option>9</option>
+				<option>10</option>
+				<option>11</option>
+				<option>12</option>
 			</select>
-			週間
+			月
 	
+			<select class="select_day">
+				<option>なし</option>
+				<option>1</option>
+				<option>2</option>
+				<option>3</option>
+				<option>4</option>
+				<option>5</option>
+				<option>6</option>
+				<option>7</option>
+				<option>8</option>
+				<option>9</option>
+				<option>10</option>
+				<option>11</option>
+				<option>12</option>
+				<option>13</option>
+				<option>14</option>
+				<option>15</option>
+				<option>16</option>
+				<option>17</option>
+				<option>18</option>
+				<option>19</option>
+				<option>20</option>
+				<option>21</option>
+				<option>22</option>
+				<option>23</option>
+				<option>24</option>
+				<option>25</option>
+				<option>26</option>
+				<option>27</option>
+				<option>28</option>
+				<option>29</option>
+				<option>30</option>
+				<option>31</option>
+			</select>
+			日
 
+			<select class="select_day">
+				<option>なし</option>
+				<option>1</option>
+				<option>2</option>
+				<option>3</option>
+				<option>4</option>
+				<option>5</option>
+				<option>6</option>
+				<option>7</option>
+				<option>8</option>
+				<option>9</option>
+				<option>10</option>
+				<option>11</option>
+				<option>12</option>
+				<option>13</option>
+				<option>14</option>
+				<option>15</option>
+				<option>16</option>
+				<option>17</option>
+				<option>18</option>
+				<option>19</option>
+				<option>20</option>
+				<option>21</option>
+				<option>22</option>
+				<option>23</option>
+				<option>24</option>
+			</select>
+			時
+			
+			</p>
+			<p>
+			おきに
+
+			<select class="select_year">
+				<option>なし</option>
+				<option>2012</option>
+				<option>2013</option>
+				<option>2014</option>
+			</select>
+			年
+
+			<select class="select_day">
+				<option>なし</option>
+				<option>1</option>
+				<option>2</option>
+				<option>3</option>
+				<option>4</option>
+				<option>5</option>
+				<option>6</option>
+				<option>7</option>
+				<option>8</option>
+				<option>9</option>
+				<option>10</option>
+				<option>11</option>
+				<option>12</option>
+			</select>
+			月
+	
+			<select class="select_day">
+				<option>なし</option>
+				<option>1</option>
+				<option>2</option>
+				<option>3</option>
+				<option>4</option>
+				<option>5</option>
+				<option>6</option>
+				<option>7</option>
+				<option>8</option>
+				<option>9</option>
+				<option>10</option>
+				<option>11</option>
+				<option>12</option>
+				<option>13</option>
+				<option>14</option>
+				<option>15</option>
+				<option>16</option>
+				<option>17</option>
+				<option>18</option>
+				<option>19</option>
+				<option>20</option>
+				<option>21</option>
+				<option>22</option>
+				<option>23</option>
+				<option>24</option>
+				<option>25</option>
+				<option>26</option>
+				<option>27</option>
+				<option>28</option>
+				<option>29</option>
+				<option>30</option>
+				<option>31</option>
+			</select>
+			日
+			</p>			
+			からメールを
 
 		</div>
 
