3e86138ad2509cd8be7671686568a87b905373ef
tooooomin <kia.xs2x.4b8sw@gmail.com>
2012-12-25 15:25:31 +0900

    design

---
+++ view/mypage.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Bootstrap 101 Template</title>
+		<!-- Bootstrap -->
+		<link href="../core/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+		<link href="../core/css/mypage.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<script src="http://code.jquery.com/jquery-latest.js"></script>
+		<script src="../core/bootstrap/js/bootstrap.min.js"></script>
+	
+		<div class="container-narrow">
+			<div class="masthead">
+				<ul class="nav nav-pills pull-right">
+					<li class="active"><a href="#">Home</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+				<h3 class="muted">Project name</h3>
+			</div>
+			<hr>
+			<h1>みみかきおしらせったー</h1>
+			<textarea cols="10" rows="1" width="400" wrap="off">todoを入力</textarea>
+		
+		
+		</div>
+
+	</body>
+</html>
+
